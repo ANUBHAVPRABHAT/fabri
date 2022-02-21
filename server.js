98762d76da3bb7289700db40480556d5f2a0fccf
ANUBHAV PRABHAT KUAMR SINGH <99394344+ANUBHAVPRABHAT@users.noreply.github.com>
2022-02-21 19:53:34 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express"); //lib-----Node Module
+var app = express(); // will create app object from express package
+
+var onAboutUs = function(req, res) {
+    res.send("By:Anubhav Prabhat");
+};
+var onDefault = function(req, res) {
+        res.send("<h1>Fabrikam</h1>" +
+            "<hr/>"+
+            "<p>Fabrikam is a multinational company has their 24 Offices all over world. Company wanted to build and host Information Management Solutions in web. Company also wants to host internet public web site for their potential customers to know about products and services provided. They required to build and host intranet application for all employees to access Human Resource, Accounting and Project Management related information. Fabrikam has public Cloud subscription to use it as Infrastructure as a Service (I-a-a-S) and Platform As A Service.</>")
+			};
+        app.get("/", onDefault); // Request handler functions are registered
+        app.get("/aboutus", onAboutUs); // Request handler functions are registered
+        var server = app.listen(8081);
+        console.log("Server is running on port 8081");
